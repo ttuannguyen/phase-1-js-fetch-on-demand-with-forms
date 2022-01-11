e6e4506ea60ba8669d2f513367474238748f1d6e
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-01-10 22:35:21 -0500

    commit tree be65b3bf9c302bfb49d1594b313da5abf81eb01f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+//Add Event Listeners to Capture Form Data and Override the Form's Behavior
 const init = () => {
-  
+    const inputForm = document.querySelector('form'); //add an event listener to the form element
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //method to override the form's behavior and prevent refreshing 
+        const input = document.querySelector('input#searchByID');
+        
+        //console.log(input.value);
+
+        //Fetch Data Based on User Input
+        fetch(`http://localhost:3000/movies/${input.value}`)//to modify the URL we pass to our fetch function based on the input typed into the HTML form
+        .then(response => response.json())
+        // .then(data => {
+        //     console.log(data); // LOG: (3) [{…}, {…}, {…}]
+        // });
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            //change the contents of our title and summary elements based on the retrieved data
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
